1ffbc0b9c55087391c942ab006ff59657b59690a
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2022-07-12 11:49:20 +0500

    Create theme

---
+++ src/theme.ts
@@ -0,0 +1,35 @@
+const theme = {
+  colors: {
+    orange: {
+      100: "#E84545",
+      200: "#903749",
+      300: "#53354A",
+      400: "#2F1D29",
+    },
+    blue: {
+      100: "#34385A",
+      200: "#2B2E4A",
+    },
+    green: {
+      100: "#6EBF8B",
+    },
+    yellow: {
+      100: "#FFE59D",
+    },
+  },
+  font: {
+    family: "'Red Hat Display', sans-serif",
+    weight: {
+      regular: 400,
+      medium: 500,
+      bold: 700,
+    },
+    size: {
+      desktop: "24px",
+      mobile: "18px",
+    },
+  },
+};
+
+export default theme;
+export type ThemeType = typeof theme;
