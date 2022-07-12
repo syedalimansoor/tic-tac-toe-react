9a87eaaf8993b037d936358734d7b600525d8d87
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2022-07-12 18:10:56 +0500

    Create Start route and style background

--- src/routes/Start.tsx
+++ src/routes/Start.tsx
@@ -1,5 +1,29 @@
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+import oDesktop from "$/assets/o-desktop-orange.svg";
+import xDesktop from "$/assets/x-desktop-orange.svg";
+import oMobile from "$/assets/o-mobile-orange.svg";
+import xMobile from "$/assets/x-mobile-orange.svg";
+
+const Container = styled.div`
+  height: 100%;
+  background-color: ${({ theme }) => theme.colors.blue[200]};
+  display: grid;
+  place-content: center;
+  grid-template-rows: 1fr auto 1fr;
+  background-image: url(${xMobile}), url(${oMobile});
+  background-repeat: no-repeat;
+  /* background-position: top calc(-100px + 10vw) left calc(-100px + 10vw),
+    top calc(-100px + 10vw) right calc(-100px + 10vw); */
+  background-position: top -50px left -50px, top -50px right -50px;
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.medium}) {
+    background-image: url(${xDesktop}), url(${oDesktop});
+    background-position: top -200px left -200px, bottom -200px right -200px;
+  }
+`;
 
 export default function Start() {
-  return <div></div>;
+  return <Container>Hello</Container>;
 }
