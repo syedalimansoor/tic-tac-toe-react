790ffb0f0b027b0f47d667777d4b09cf752558e9
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2022-07-12 14:09:10 +0500

    Set up react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import theme from "./style/theme";
 import { ThemeProvider } from "styled-components";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Start from "./routes/Start";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Start />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
---
+++ src/routes/Start.tsx
@@ -0,0 +1,5 @@
+import { Link } from "react-router-dom";
+
+export default function Start() {
+  return <div></div>;
+}
