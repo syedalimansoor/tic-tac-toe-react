354f0f1875fa5423d6f59c67b25be126f3e4a3ab
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2022-07-12 14:25:04 +0500

    Create global styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import theme from "./style/theme";
 import { ThemeProvider } from "styled-components";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Start from "./routes/Start";
+import GlobalStyles from "./style/global";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyles />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Start />} />
---
+++ src/style/global.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  *, *::before, *::after {
+    margin: 0;
+    padding: 0;
+  }
+  
+  ::root {
+    font-family: ${({ theme }) => theme.font.family};
+    font-size: ${({ theme }) => theme.font.size.mobile};
+    font-weight: ${({ theme }) => theme.font.weight.regular}
+  }
+
+  `;
+
+export default GlobalStyles;
