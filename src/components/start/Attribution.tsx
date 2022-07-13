4a28a3d05d5011ba701cb4a815fd030be47561de
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2022-07-13 18:57:58 +0500

    Add animation to attribution and link to IG page

--- src/components/start/Attribution.tsx
+++ src/components/start/Attribution.tsx
@@ -1,12 +1,33 @@
+import { motion } from "framer-motion";
 import styled from "styled-components";
 
-const Text = styled.p`
+const Text = styled(motion.p)`
   grid-row: 3;
   align-self: end;
   color: ${({ theme }) => theme.colors.orange[200]};
   font-weight: ${({ theme }) => theme.font.weight.medium};
 `;
 
+const Link = styled.a`
+  color: ${({ theme }) => theme.colors.orange[200]};
+  transition: 200ms ease;
+
+  &:hover {
+    color: ${({ theme }) => theme.colors.orange[100]};
+  }
+`;
+
 export default function Attribution() {
-  return <Text>Made by Syed Ali Mansoor</Text>;
+  return (
+    <Text
+      initial={{ y: 50, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 1, type: "tween" }}
+    >
+      Made by{" "}
+      <Link href="https://www.instagram.com/alimansoor.dev" target={"_blank"}>
+        Syed Ali Mansoor
+      </Link>
+    </Text>
+  );
 }
