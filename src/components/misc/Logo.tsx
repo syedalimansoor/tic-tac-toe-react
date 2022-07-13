65455155e47f44caa11ff158056e31fc91ce4783
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2022-07-13 21:34:18 +0500

    Create Settings and Logo

---
+++ src/components/misc/Logo.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Text = styled.h1`
+  text-align: center;
+`;
+
+export default function Logo() {
+  return <Text>tic-tac-toe.</Text>;
+}
---
+++ src/routes/Settings.tsx
@@ -0,0 +1,36 @@
+import pageTransition from "$/utils/pageTransition";
+import { motion } from "framer-motion";
+import styled from "styled-components";
+
+import xMobile from "$/assets/x-mobile-blue.svg";
+import xDesktop from "$/assets/x-desktop-blue.svg";
+import oMobile from "$/assets/o-mobile-blue.svg";
+import oDesktop from "$/assets/o-desktop-blue.svg";
+import Logo from "$/components/misc/Logo";
+
+const Container = styled(motion.div)`
+  height: 100%;
+  background-image: url(${xMobile}), url(${oMobile});
+  background-repeat: no-repeat;
+  background-position: top -50px left -50px, top -50px right -50px;
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.medium}) {
+    background-image: url(${xDesktop}), url(${oDesktop});
+    background-position: top -200px left -200px, bottom -200px right -200px;
+  }
+
+  padding: 3rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: stretch;
+  gap: 2rem;
+`;
+
+export default function Settings() {
+  return (
+    <Container {...pageTransition}>
+      <Logo />
+    </Container>
+  );
+}
