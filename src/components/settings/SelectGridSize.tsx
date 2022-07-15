bfd11c5ea7d04c2bbc21821f9b132b2a3317e837
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2022-07-15 11:17:20 +0500

    Complete GridSize

--- src/components/settings/SelectGridSize.tsx
+++ src/components/settings/SelectGridSize.tsx
@@ -1,3 +1,34 @@
+import { selectGridSize } from "$/features/gridSize";
+import useStore from "$/store";
+import SelectOption from "./SelectOption";
+import { Label, OptionsList } from "./styled";
+
 export default function SelectGridSize() {
-  return <div>SelectGridSize</div>;
+  const { gridSize, setGridSize } = useStore(selectGridSize);
+
+  return (
+    <>
+      <Label>Grid size:</Label>
+      <OptionsList>
+        <SelectOption
+          group="gridSize"
+          value="3"
+          checked={gridSize === 3}
+          onChange={() => setGridSize(3)}
+        />
+        <SelectOption
+          group="gridSize"
+          value="4"
+          checked={gridSize === 4}
+          onChange={() => setGridSize(4)}
+        />
+        <SelectOption
+          group="gridSize"
+          value="5"
+          checked={gridSize === 5}
+          onChange={() => setGridSize(5)}
+        />
+      </OptionsList>
+    </>
+  );
 }
