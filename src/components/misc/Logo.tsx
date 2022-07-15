f5f8c37f507bd41e2e5f03fa0bc3284c7cf8fd3f
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2022-07-15 16:50:11 +0500

    Allow logo size to be controlled by parent

--- src/components/misc/Logo.tsx
+++ src/components/misc/Logo.tsx
@@ -1,9 +1,13 @@
 import styled from "styled-components";
 
+interface Props {
+  className?: string;
+}
+
 const Text = styled.h1`
   text-align: center;
 `;
 
-export default function Logo() {
-  return <Text>tic-tac-toe.</Text>;
+export default function Logo(props: Props) {
+  return <Text className={props.className}>tic-tac-toe.</Text>;
 }
