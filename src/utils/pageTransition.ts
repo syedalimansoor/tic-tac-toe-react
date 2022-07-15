30aee117f06385ed4f143750d21e80f0632890fc
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2022-07-15 19:09:30 +0500

    Add delay in page transitions

--- src/utils/pageTransition.ts
+++ src/utils/pageTransition.ts
@@ -2,7 +2,7 @@ const pageTransition = {
   initial: { opacity: 0 },
   animate: { opacity: 1 },
   exit: { opacity: 0 },
-  transition: { duration: 0.5 },
+  transition: { duration: 0.5, delay: 0.2 },
 };
 
 export default pageTransition;
