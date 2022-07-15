ab35ff5aec33a00c4d843fd327e456c37a3b4fd1
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2022-07-15 10:43:07 +0500

    Create file to store styled components

---
+++ src/components/settings/styled.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Label = styled.h2`
+  font-size: 1rem;
+`;
+
+export const OptionsList = styled.div`
+  display: flex;
+  justify-content: stretch;
+  gap: 1em;
+`;
