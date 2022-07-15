54ddee146c57917a33119831fa247ae4e48b80b1
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2022-07-15 10:43:26 +0500

    Create SelectGameMode

--- src/components/settings/SelectGameMode.tsx
+++ src/components/settings/SelectGameMode.tsx
@@ -1,3 +1,28 @@
+import { selectGameMode } from "$/features/gameMode";
+import useStore from "$/store";
+import SelectOption from "./SelectOption";
+import { Label, OptionsList } from "./styled";
+
 export default function SelectGameMode() {
-  return <div>SelectGameMode</div>;
+  const { gameMode, setGameMode } = useStore(selectGameMode);
+
+  return (
+    <>
+      <Label>Game mode:</Label>
+      <OptionsList>
+        <SelectOption
+          group="gameMode"
+          value="PvC"
+          checked={gameMode === "PvC"}
+          onChange={() => setGameMode("PvC")}
+        />
+        <SelectOption
+          group="gameMode"
+          value="PvP"
+          checked={gameMode === "PvP"}
+          onChange={() => setGameMode("PvP")}
+        />
+      </OptionsList>
+    </>
+  );
 }
