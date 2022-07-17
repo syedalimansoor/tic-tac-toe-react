df678984c69f7d2100eee59ecc6c9bf5914cec65
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2022-07-17 23:32:51 +0500

    Create PlayerDisplay

--- src/components/game/GameBody.tsx
+++ src/components/game/GameBody.tsx
@@ -1,3 +1,5 @@
+import { selectPlayers } from "$/features/players";
+import useStore from "$/store";
 import styled from "styled-components";
 import CurrentPlayer from "./CurrentPlayer";
 import Grid from "./Grid";
@@ -21,16 +23,29 @@ const Container = styled.div`
   align-content: space-between;
 `;
 
+const PlayersContainer = styled.div`
+  display: flex;
+  width: min(100%, 20em);
+  padding-inline: 1em;
+  justify-content: stretch;
+  gap: 3em;
+  & > * {
+    flex: 1 0 0;
+  }
+`;
+
 export default function GameBody() {
+  const { players } = useStore(selectPlayers);
+
   return (
     <Wrapper>
       <Container>
         <Score />
         <Grid />
-        <div>
-          <PlayerDisplay />
-          <PlayerDisplay />
-        </div>
+        <PlayersContainer>
+          <PlayerDisplay player={players[0]} />
+          <PlayerDisplay player={players[1]} />
+        </PlayersContainer>
         <CurrentPlayer />
       </Container>
     </Wrapper>
--- src/components/game/PlayerDisplay.tsx
+++ src/components/game/PlayerDisplay.tsx
@@ -1,3 +1,35 @@
-export default function PlayerDisplay() {
-  return <div>PlayerDisplay</div>;
+import { Mark, Player } from "$/features/players";
+import styled from "styled-components";
+
+import xDesktop from "$/assets/x-desktop-blue.svg";
+import oDesktop from "$/assets/o-desktop-blue.svg";
+import xMobile from "$/assets/x-mobile-blue.svg";
+import oMobile from "$/assets/o-mobile-blue.svg";
+
+const background = {
+  mobile: { X: xMobile, O: oMobile },
+  desktop: { X: xDesktop, O: oDesktop },
+};
+
+interface Props {
+  player: Player;
+}
+
+const Display = styled.div<{ mark: Mark }>`
+  font-size: 1.4rem;
+  font-weight: ${({ theme }) => theme.font.weight.bold};
+  aspect-ratio: 1;
+  display: grid;
+  place-items: center;
+  background-image: url(${({ mark }) => background.mobile[mark]});
+  background-size: cover;
+  background-repeat: no-repeat;
+`;
+
+export default function PlayerDisplay(props: Props) {
+  return (
+    <Display mark={props.player.mark}>
+      {props.player.name || `Player ${props.player.mark}`}
+    </Display>
+  );
 }
