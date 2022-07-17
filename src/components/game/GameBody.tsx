9267f147d60b97e3efc89727bcfd188b3c6a4781
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2022-07-17 22:09:48 +0500

    Create wrappers

--- src/components/game/GameBody.tsx
+++ src/components/game/GameBody.tsx
@@ -1,18 +1,32 @@
+import styled from "styled-components";
 import CurrentPlayer from "./CurrentPlayer";
 import Grid from "./Grid";
 import PlayerDisplay from "./PlayerDisplay";
 import Score from "./Score";
 
+const Wrapper = styled.main`
+  padding: 1em;
+  display: flex;
+  justify-content: center;
+`;
+const Container = styled.div`
+  width: min(100%, 25em);
+  display: grid;
+  justify-items: center;
+`;
+
 export default function GameBody() {
   return (
-    <div>
-      <Score />
-      <Grid />
-      <div>
-        <PlayerDisplay />
-        <PlayerDisplay />
-      </div>
-      <CurrentPlayer />
-    </div>
+    <Wrapper>
+      <Container>
+        <Score />
+        <Grid />
+        <div>
+          <PlayerDisplay />
+          <PlayerDisplay />
+        </div>
+        <CurrentPlayer />
+      </Container>
+    </Wrapper>
   );
 }
