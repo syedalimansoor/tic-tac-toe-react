b6d2671a4bbfd07f927ba5664586a61c6eebf997
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2022-07-26 15:42:16 +0500

    Create grid cell click handler and reset game action

--- src/store.ts
+++ src/store.ts
@@ -3,16 +3,20 @@ import { devtools } from "zustand/middleware";
 import { StateCreatorWithMiddleware } from "./utils/StateCreatorWithMiddleware";
 import createGameModeSlice, { GameModeSlice } from "./features/gameMode";
 import createGridSlice, { GridSlice } from "./features/grid";
-import createPlayersSlice, { PlayersSlice } from "./features/players";
+import createPlayersSlice, { Mark, PlayersSlice } from "./features/players";
 import createMovesSlice, { MovesSlice } from "./features/moves";
 import createGameStateSlice, { GameStateSlice } from "./features/gameState";
+import checkForMatch from "./utils/checkForMatch";
 
 export interface RootState
   extends GameModeSlice,
     GridSlice,
     PlayersSlice,
     MovesSlice,
-    GameStateSlice {}
+    GameStateSlice {
+  handleGridCellClick: (rowIdx: number, colIdx: number, newMark: Mark) => void;
+  resetGame: () => void;
+}
 
 const applyMiddleware = (f: StateCreatorWithMiddleware<RootState>) =>
   devtools(f);
@@ -24,6 +28,47 @@ const useStore = create<RootState>()(
     ...createPlayersSlice(...args),
     ...createMovesSlice(...args),
     ...createGameStateSlice(...args),
+
+    handleGridCellClick(rowIdx, colIdx, newMark) {
+      const set = args[0];
+      const get = args[1];
+      const grid = get().grid;
+      const gameState = get().gameState;
+
+      // Terminate if:
+      // - Given cell is already marked
+      if (grid[rowIdx][colIdx].mark) return;
+      // - Game is over
+      if (gameState.isGameOver) return;
+
+      get().markGrid(rowIdx, colIdx, newMark);
+      get().incrementMoves();
+      get().updateGridCounters(rowIdx, colIdx, newMark);
+
+      // Check if there have been enough moves to make a match
+      const gridSize = get().gridSize;
+      const numMoves = get().numMoves;
+      if (numMoves >= gridSize * 2 - 1) {
+        const gridCounters = get().gridCounters;
+        const matchDetails = checkForMatch(gridCounters, gridSize);
+
+        // Check if current counter values result in a match
+        if (matchDetails.doesMatchExist) {
+          set({ gameState: { isGameOver: true, doesMatchExist: true } });
+          get().matchGrid(matchDetails);
+        } else if (numMoves === gridSize ** 2) {
+          set({ gameState: { isGameOver: true, doesMatchExist: false } });
+        }
+      }
+    },
+
+    resetGame() {
+      const get = args[1];
+      get().resetGrid();
+      get().resetGridCounters();
+      get().resetMoves();
+      get().resetGameState();
+    },
   }))
 );
 
